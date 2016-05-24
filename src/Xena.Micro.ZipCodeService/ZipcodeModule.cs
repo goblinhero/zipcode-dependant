cba8bfc5077ba3a08ebaf8746d4b0e3a5da7d1e9
goblinhero <duckhero@gmail.com>
2016-05-24 14:16:36 +0200

    Testing stuff

--- src/Xena.Micro.ZipCodeService/ZipcodeModule.cs
+++ src/Xena.Micro.ZipCodeService/ZipcodeModule.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Nancy;
 using Nancy.Responses;
 
@@ -14,8 +16,17 @@ namespace Xena.Micro.ZipCodeService
             Get["/"] = p => GetService();
         }
 
-        private object GetService()
+        private async Task<object> GetService()
         {
+            using (var client = new HttpClient())
+            {
+                var req = await client.GetAsync("http://zipcode-1/DK/Zip/9000");
+                if (req.IsSuccessStatusCode)
+                {
+                    var str = await req.Content.ReadAsStringAsync();
+                    return str;
+                }
+            }
             return Environment.GetEnvironmentVariables();
         }
 
@@ -24,12 +35,12 @@ namespace Xena.Micro.ZipCodeService
             var country = new Country(countryCode);
             IList<Zipcode> zipCodes;
             IEnumerable<string> errors;
-            if(_repository.GetZipByCountry(country, out zipCodes, out errors))
+            if (_repository.GetZipByCountry(country, out zipCodes, out errors))
                 return zipCodes;
-            return new NotAcceptableResponse {ReasonPhrase = string.Join(Environment.NewLine,errors)};
+            return new NotAcceptableResponse { ReasonPhrase = string.Join(Environment.NewLine, errors) };
         }
 
-        private object GetZipcode(string countryCode,string zip)
+        private object GetZipcode(string countryCode, string zip)
         {
             var country = new Country(countryCode);
             Zipcode zipcode;
