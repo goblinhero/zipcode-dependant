28a45e73a9c742802559e6f26526d86a2761d3fe
goblinhero <duckhero@gmail.com>
2016-05-26 13:18:54 +0200

    Entity => Dto

--- src/Xena.Micro.ZipCodeService/Zipcode.cs
+++ src/Xena.Micro.ZipCodeService/Zipcode.cs
@@ -2,14 +2,6 @@ namespace Xena.Micro.ZipCodeService
 {
     public class Zipcode
     {
-        public Zipcode(string zip, string city, string additionalInfo, Country country)
-        {
-            Zip = zip;
-            City = city;
-            AdditionalInfo = additionalInfo;
-            Country = country;
-        }
-
         public string Zip { get; set; }
         public string City { get; set; }
         public string AdditionalInfo { get; set; }
